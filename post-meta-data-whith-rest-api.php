c4933b4237497ff058959fc73707dcbe4e4c9643
NBY <nby@nby.me>
2021-06-22 21:52:52 +0900

    Update post-meta-data-whith-rest-api.php

--- post-meta-data-whith-rest-api.php
+++ post-meta-data-whith-rest-api.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Post Meta Data Whith REST API
  * Plugin URI:  https://github.com/NBY/Post-Meta-Data-Whith-REST-API
- * Description: Allows add meta data when posting posts using rest api.Example code please visit:https://github.com/NBY/Post-Meta-Data-Whith-REST-API
+ * Description: Allows add meta data when posting posts using rest api.Example code please visit:https://github.com/NBY/Post-Meta-Data-Whith-REST-API If this plugin helps you, please give me a star. Thank you!
  * Version:     0.0.1
  * Author:      Alexander Nie
  * Author URI:  https://nby.me
